849dfd8a1255d0d3b058fe7c1856280b0d3479a9
mang-jin <catkinggo113@gmail.com>
2026-05-09 18:14:38 +0900

    오랜만에 config 조정이군

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+
+vim.opt.list = true
+vim.opt.listchars = {
+    space = '·',
+    tab = '> '
+}
